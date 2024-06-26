61f8a54360dc06679588c120ac8613494ebaf533
mo-oloo <72270742+mo-oloo@users.noreply.github.com>
2024-06-25 21:26:42 -0500

    chore: cleared files not currently in use

--- src/physics.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-INF = float('inf')
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,9 +1,3 @@
 '''
 This file will handle generating parameters for multiple systems and running them in parallel.
 '''
-
-def generate_parameters():
-    '''
-    This function will generate parameters for multiple systems.
-    '''
-    pass
